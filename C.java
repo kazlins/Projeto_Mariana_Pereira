3c6c87765cce8f48a27a20fc7439e230de15b767
Mariana Lucas <mariana.lucas@estudante.ufla.br>
2026-04-18 15:11:47 -0300

    Criação da classe C

---
+++ C.java
@@ -0,0 +1,14 @@
+public class C {
+    private String C1;
+    private Int C2;
+
+
+    public void MC1(){
+        System.out.println("Metodo MC1");
+    }
+    
+    public void MC2(){
+        System.out.println("Metodo MC2");
+    }
+
+}
